c3800068a9fc7c20d4005125a1ec73b06ab91535
Aguilarite <99771157+Aguilarite@users.noreply.github.com>
2023-05-11 04:15:47 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const multer = require('multer');
+
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, '<THE DIRECTORY TO SAVE AND STORE FILES RESIVED>')
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.originalname)
+    }
+});
+
+var upload = multer({ storage: storage })
+
+app.post('/uploaded', upload.single('upload'), function(req, res, next) {
+    console.log(req.body.description);
+    res.status(200).send({ 'answer': "file uploaded" });
+});
+
+app.use('/uploaded', express.static('<THE DIRECTORY TO SAVE AND STORE FILES RESIVED>'));
